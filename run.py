7646ee1dd750c61f84635e6dc51250f616c45c23
Mugo Patricia <141113395+simplymuthoni@users.noreply.github.com>
2024-06-02 14:54:06 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+
+from app import create_app
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config_name = os.getenv('FLASK_ENV')
+print("Config name: ", config_name)
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
